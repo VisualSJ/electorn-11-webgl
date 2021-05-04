00a2d7125ee0df58f293a6a053c78fc99a2a8be6
SijieWang <itharbors@outlook.com>
2021-05-04 19:08:13 +0800

    123

--- workflow.js
+++ workflow.js
@@ -1,7 +1,7 @@
 const { fstat } = require("fs");
 new Promise((resolve) => {
     console.log('mmmmmmmmmmmmmm')
-    require('fs').writeFileSync('./tt.log', '123');
-    process.exit(-1);
+    // require('fs').writeFileSync('./tt.log', '123');
+    // process.exit(-1);
     setTimeout(resolve, 10000);
 });
